487ced317e48e68440d609a8a3eb53393f8d0c01
Ewart Timothée <ewart@bluebrain12.epfl.ch>
2013-09-17 12:15:24 +0200

    modify bench for  Fabien

--- src/main/mem.cpp
+++ src/main/mem.cpp
@@ -38,47 +38,43 @@ static __inline__ unsigned long long rdtsc(void) {
 
 int main(){
     
-    
-      
-    memory::block<TYPE,16,1024,memory::AoS> block_a;
-    memory::block<TYPE,16,1024,memory::AoSoA> block_b;
+  memory::block<TYPE,16,1024,memory::AoS> block_a;
+  memory::block<TYPE,16,1024,memory::AoSoA> block_b;
 
-    typename memory::block<TYPE,16,1024,memory::AoS>::iterator it_AoS = block_a.begin();
-    for(; it_AoS != block_a.end(); ++it_AoS){
-        for(int i=0  ; i<16 ; ++i)
-            (*it_AoS)[0] = GetRandom<TYPE>();
-    }
+  TYPE random;
 
-    typename memory::block<TYPE,16,1024,memory::AoSoA>::iterator it_AoSoA = block_b.begin();
-    for(; it_AoSoA != block_b.end(); ++it_AoSoA){
-        for(int i=0  ; i<16 ; ++i)
-            *(*it_AoSoA)[0] = GetRandom<TYPE>();
-    }
+  for(int i=0; i <1024; ++i)
+      for(int j=0; j <16; ++j){
+              random  = GetRandom<TYPE>();
+              block_a(i,j) = random;
+              block_b(i,j) = random;
 
+      }
+
+  typename memory::block<TYPE,16,1024,memory::AoS>::iterator it_AoS = block_a.begin();
+  typename memory::block<TYPE,16,1024,memory::AoSoA>::iterator it_AoSoA = block_b.begin();
 
   unsigned long long a,b,c,d;
 
 
   a = rdtsc();
     for(it_AoS = block_a.begin(); it_AoS != block_a.end(); ++it_AoS){
-           numeric::add<TYPE>((*it_AoS)[15],(*it_AoS)[1]);
-           numeric::mul<TYPE>((*it_AoS)[14],(*it_AoS)[2]);
-           numeric::add<TYPE>((*it_AoS)[13],(*it_AoS)[3]);
-           numeric::exp<TYPE,14>((*it_AoS)[12],(*it_AoS)[9]);
-           numeric::add<TYPE>((*it_AoS)[12],(*it_AoS)[4]);
-           numeric::add<TYPE>((*it_AoS)[11],(*it_AoS)[5]);
-           numeric::add<TYPE>((*it_AoS)[10],(*it_AoS)[6]);
-           numeric::add<TYPE>((*it_AoS)[9],(*it_AoS)[7]);
-           numeric::add<TYPE>((*it_AoS)[8],(*it_AoS)[8]);
-           numeric::mul<TYPE>((*it_AoS)[7],(*it_AoS)[9]);
-           numeric::exp<TYPE,14>((*it_AoS)[12],(*it_AoS)[8]);
-           numeric::mul<TYPE>((*it_AoS)[6],(*it_AoS)[11]);
-           numeric::mul<TYPE>((*it_AoS)[5],(*it_AoS)[12]);
-           numeric::mul<TYPE>((*it_AoS)[4],(*it_AoS)[13]);
-           numeric::mul<TYPE>((*it_AoS)[3],(*it_AoS)[14]);
-           numeric::exp<TYPE,14>((*it_AoS)[12],(*it_AoS)[7]);
-           numeric::mul<TYPE>((*it_AoS)[2],(*it_AoS)[15]);
-           numeric::exp<TYPE,14>((*it_AoS)[2],(*it_AoS)[15]);
+           numeric::exp<TYPE,14>((*it_AoS)[15],(*it_AoS)[0]);
+           numeric::exp<TYPE,14>((*it_AoS)[14],(*it_AoS)[1]);
+           numeric::exp<TYPE,14>((*it_AoS)[13],(*it_AoS)[2]);
+           numeric::exp<TYPE,14>((*it_AoS)[12],(*it_AoS)[3]);
+           numeric::exp<TYPE,14>((*it_AoS)[11],(*it_AoS)[4]);
+           numeric::exp<TYPE,14>((*it_AoS)[10],(*it_AoS)[5]);
+           numeric::exp<TYPE,14>((*it_AoS)[9],(*it_AoS)[6]);
+           numeric::exp<TYPE,14>((*it_AoS)[8],(*it_AoS)[7]);
+           numeric::exp<TYPE,14>((*it_AoS)[7],(*it_AoS)[8]);
+           numeric::exp<TYPE,14>((*it_AoS)[6],(*it_AoS)[9]);
+           numeric::exp<TYPE,14>((*it_AoS)[5],(*it_AoS)[10]);
+           numeric::exp<TYPE,14>((*it_AoS)[4],(*it_AoS)[11]);
+           numeric::exp<TYPE,14>((*it_AoS)[3],(*it_AoS)[12]);
+           numeric::exp<TYPE,14>((*it_AoS)[2],(*it_AoS)[13]);
+           numeric::exp<TYPE,14>((*it_AoS)[1],(*it_AoS)[14]);
+           numeric::exp<TYPE,14>((*it_AoS)[0],(*it_AoS)[15]);
     }   
   b = rdtsc();
 
@@ -86,28 +82,29 @@ int main(){
 
   c = rdtsc();
     for(it_AoSoA = block_b.begin(); it_AoSoA != block_b.end(); ++it_AoSoA){
-           numeric::add<TYPE>((*it_AoSoA)[15],(*it_AoSoA)[1]);
-           numeric::mul<TYPE>((*it_AoSoA)[14],(*it_AoSoA)[2]);
-           numeric::add<TYPE>((*it_AoSoA)[13],(*it_AoSoA)[3]);
-           numeric::exp<TYPE,14>((*it_AoSoA)[12],(*it_AoSoA)[9]);
-           numeric::add<TYPE>((*it_AoSoA)[12],(*it_AoSoA)[4]);
-           numeric::add<TYPE>((*it_AoSoA)[11],(*it_AoSoA)[5]);
-           numeric::add<TYPE>((*it_AoSoA)[1],(*it_AoSoA)[6]);
-           numeric::add<TYPE>((*it_AoSoA)[9],(*it_AoSoA)[7]);
-           numeric::add<TYPE>((*it_AoSoA)[8],(*it_AoSoA)[8]);
-           numeric::mul<TYPE>((*it_AoSoA)[7],(*it_AoSoA)[9]);
-           numeric::exp<TYPE,14>((*it_AoSoA)[12],(*it_AoSoA)[8]);
-           numeric::mul<TYPE>((*it_AoSoA)[6],(*it_AoSoA)[11]);
-           numeric::mul<TYPE>((*it_AoSoA)[5],(*it_AoSoA)[12]);
-           numeric::mul<TYPE>((*it_AoSoA)[4],(*it_AoSoA)[13]);
-           numeric::mul<TYPE>((*it_AoSoA)[3],(*it_AoSoA)[14]);
-           numeric::exp<TYPE,14>((*it_AoSoA)[12],(*it_AoSoA)[7]);
-           numeric::mul<TYPE>((*it_AoSoA)[2],(*it_AoSoA)[15]);
-           numeric::exp<TYPE,14>((*it_AoSoA)[2],(*it_AoSoA)[15]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[15],(*it_AoSoA)[0]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[14],(*it_AoSoA)[1]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[13],(*it_AoSoA)[2]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[12],(*it_AoSoA)[3]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[11],(*it_AoSoA)[4]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[10],(*it_AoSoA)[5]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[9],(*it_AoSoA)[6]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[8],(*it_AoSoA)[7]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[7],(*it_AoSoA)[8]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[6],(*it_AoSoA)[9]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[5],(*it_AoSoA)[10]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[4],(*it_AoSoA)[11]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[3],(*it_AoSoA)[12]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[2],(*it_AoSoA)[13]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[1],(*it_AoSoA)[14]);
+           numeric::exp<TYPE,14>((*it_AoSoA)[0],(*it_AoSoA)[15]);
     }   
   d = rdtsc();
-
-
+/*
+  for(int i=0; i <1024; ++i)
+      for(int j=0; j <16; ++j)
+          std::cout << block_a(i,j) << " " << block_b(i,j) << std::endl;
+*/
  printf("%llu\n", d-c);
 
 
