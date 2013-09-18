7a1b41aac8a86971af666942f87cd4d4163acfa7
Ewart Timothée <ewart@bbpviz1.cscs.ch>
2013-09-18 17:36:52 +0200

    fix bench test cases

--- src/main/bench.cpp
+++ src/main/bench.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>  
 #include <limits>  
+#include <string>  
 
 #include <vector>
 
@@ -19,93 +20,57 @@
 #include "numeric/math/math.hpp"
 #include "memory/block.hpp"
 #include "utils/timer.h"
+#include "helper.h"
 
-#define SIZE 128*128
-
-//random generator
-static boost::random::uniform_real_distribution<float>    RandomDouble = boost::random::uniform_real_distribution<float>(-5,5);
-static boost::random::uniform_real_distribution<double>   Randomfloat  = boost::random::uniform_real_distribution<double>(-5,5);
-static boost::random::mt19937    rng;
-
-template<class T>
-T GetRandom();
-
-template<>
-float GetRandom<float>(){
-    return Randomfloat(rng);
-}
-
-template<>
-double GetRandom<double>(){
-    return RandomDouble(rng);
-}
-
-template<class T, memory::order O>
-struct Solver_add;
-
-template<class T>
-struct Solver_add<T, memory::AoS>{
-   typedef T value_type;
-   static inline void add(T a,T b){
-       std::cout << " je suis la " << std::endl;
-       numeric::add(a,b);
-   };   
-};
-
-typedef Solver_add<float,memory::AoS>  add_s;
-
-typedef boost::mpl::vector<add_s> Solver_op_list;
+#define SIZE 1024
+#define M 16
 
 struct test_case{
     //redefine the operator() for boost mpl
     template<typename Solver>
     void operator()(Solver const&){
 
-        memory::block<typename Solver::value_type,16,1024,memory::AoS> block;
+        memory::block<typename Solver::value_type,M,SIZE, Solver::O> block;
           
         typename Solver::value_type random;
 
-        for(int i=0; i <1024; ++i)
-            for(int j=0; j <16; ++j){
+        for(int i=0; i <SIZE; ++i)
+            for(int j=0; j <M; ++j){
                 random  = GetRandom<typename Solver::value_type>();
                 block(i,j) = random;
       
         }
 
-        typename memory::block<typename Solver::value_type,16,1024,memory::AoS>::iterator it = block.begin();
+       typename memory::block<typename Solver::value_type,M,SIZE, Solver::O>::iterator it = block.begin();
 
-//        boost::chrono::high_resolution_clock::time_point start1 = boost::chrono::high_resolution_clock::now();
+       timer t;
+       t.start();
+
             for(it = block.begin(); it != block.end(); ++it){
-                //Solver::add<typename Solver::value_type, memory::AoS>((*it)[15],(*it)[0]);
-                Solver::add((*it)[15],(*it)[0]);
-/*
-                Solver::add((*it)[14],(*it)[1]);
-                Solver::add((*it)[13],(*it)[2]);
-                Solver::add((*it)[12],(*it)[3]);
-                Solver::add((*it)[11],(*it)[4]);
-                Solver::add((*it)[10],(*it)[5]);
-                Solver::add((*it)[9],(*it)[6]);
-                Solver::add((*it)[8],(*it)[7]);
-                Solver::add((*it)[7],(*it)[8]);
-                Solver::add((*it)[6],(*it)[9]);
-                Solver::add((*it)[5],(*it)[10]);
-                Solver::add((*it)[4],(*it)[11]);
-                Solver::add((*it)[3],(*it)[12]);
-                Solver::add((*it)[2],(*it)[13]);
-                Solver::add((*it)[1],(*it)[14]);
-                Solver::add((*it)[0],(*it)[15]);
-*/
+                Solver::op((*it)[15],(*it)[0]);
+                Solver::op((*it)[14],(*it)[1]);
+                Solver::op((*it)[13],(*it)[2]);
+                Solver::op((*it)[12],(*it)[3]);
+                Solver::op((*it)[11],(*it)[4]);
+                Solver::op((*it)[10],(*it)[5]);
+                Solver::op((*it)[9],(*it)[6]);
+                Solver::op((*it)[8],(*it)[7]);
+                Solver::op((*it)[7],(*it)[8]);
+                Solver::op((*it)[6],(*it)[9]);
+                Solver::op((*it)[5],(*it)[10]);
+                Solver::op((*it)[4],(*it)[11]);
+                Solver::op((*it)[3],(*it)[12]);
+                Solver::op((*it)[2],(*it)[13]);
+                Solver::op((*it)[1],(*it)[14]);
+                Solver::op((*it)[0],(*it)[15]);
             }   
-//        boost::chrono::nanoseconds ns1 = boost::chrono::high_resolution_clock::now() - start1;
 
-
-//        std::cout << ns1.count() << " [s] " << std::endl;
-
+       t.stop();
+       std::cout << Solver::name() << ", " << t.get() << " [s] " <<  std::endl;
     }
 };
 
-    int main(int argc, char * argv[]){
-        //generate all variants, we call the operator() defined previously
-        boost::mpl::for_each<Solver_op_list>(test_case());
-        return 0;
-    }
+int main(int argc, char * argv[]){
+    boost::mpl::for_each<Solver_op_list>(test_case());
+    return 0;
+}
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,7 +8,6 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/mpl/for_each.hpp>
 #include <boost/mpl/vector.hpp>
-#include <boost/chrono/chrono.hpp>
 #include <boost/type_traits.hpp>
 
 #include <boost/random/mersenne_twister.hpp>
@@ -18,86 +17,38 @@
 
 //my files
 #include "numeric/math/math.hpp"
+#include "utils/timer.h"
+#include "helper.h"
 
-#define SIZE 128*128
-
-//random generator
-static boost::random::uniform_real_distribution<float>    RandomDouble = boost::random::uniform_real_distribution<float>(-5,5);
-static boost::random::uniform_real_distribution<double>   Randomfloat  = boost::random::uniform_real_distribution<double>(-5,5);
-static boost::random::mt19937    rng;
-
-template<class T>
-T GetRandom();
-
-template<>
-float GetRandom<float>(){
-    return Randomfloat(rng);
-}
-
-template<>
-double GetRandom<double>(){
-    return RandomDouble(rng);
-}
-
-//encapsulate the solver_exp for boost::mpl
-template<class T, std::size_t n>
-struct solver_exp{
-   typedef T value_type;
-   static const std::size_t n_ = n; // keep trace of n for the evaluation
-};
-
-//define different solver_exp for the boost::mpl::vector
-typedef solver_exp<float, 14>  exp_f14;
-typedef solver_exp<double, 14> exp_d14;
-
-typedef boost::mpl::vector<exp_f14, exp_d14 > solver_exp_list;
-
-template<class T>
-struct block{
-    T data[SIZE];
-};
+#define SIZE 512*16384
 
 struct test_case{
     //redefine the operator() for boost mpl
     template<typename Solver>
     void operator()(Solver const&){
-        int size_vector = 1024;
+        int size_vector = SIZE;
 
-        std::vector<block<typename Solver::value_type> > vec_input(size_vector);
-        std::vector<block<typename Solver::value_type> > vec_output_serial(size_vector);
-        std::vector<block<typename Solver::value_type> > vec_output_serial_std(size_vector);
-        std::vector<block<typename Solver::value_type> > vec_output_vec(size_vector);
+       std::vector<typename Solver::value_type > vec_input(size_vector);
+       std::vector<typename Solver::value_type > vec_output_serial(size_vector);
+       std::vector<typename Solver::value_type > vec_output_vec(size_vector);
 
-        for(long long int i(0); i < size_vector; ++i)
-            for(int j=0; j < SIZE; j++)
-                vec_input[i].data[j] = GetRandom<typename Solver::value_type>(); 
+       for(long long int i(0); i < size_vector; ++i)
+                vec_input[i] = GetRandom<typename Solver::value_type>(); 
 
+       timer t;
+       t.start();
 
-//        boost::chrono::high_resolution_clock::time_point start1 = boost::chrono::high_resolution_clock::now();
-        for(long long int i(0); i < size_vector; ++i)
-            for(int j=0; j < SIZE; j++)
-              vec_output_serial_std[i].data[j] = std::exp(vec_input[i].data[j]);
-//        boost::chrono::nanoseconds ns1 = boost::chrono::high_resolution_clock::now() - start1;
+       for(long long int i(0); i < size_vector; i += memory::stride<typename Solver::value_type, Solver::O>::helper_stride())
+          Solver::op(    helper_pointer<typename Solver::value_type, Solver::O>::get(vec_output_serial[i]),
+                         helper_pointer<typename Solver::value_type, Solver::O>::get(vec_input[i]);
 
-//        boost::chrono::high_resolution_clock::time_point start3 = boost::chrono::high_resolution_clock::now();
-        for(long long int i(0); i < size_vector; ++i)
-            for(int j=0; j < SIZE; j+=(memory::getsimd()/sizeof(typename Solver::value_type)))
-                numeric::exp<typename Solver::value_type, Solver::n_>(&vec_output_vec[i].data[j], &vec_input[i].data[j]);
-//        boost::chrono::nanoseconds ns3 = boost::chrono::high_resolution_clock::now() - start3;
-
-//        boost::chrono::high_resolution_clock::time_point start2 = boost::chrono::high_resolution_clock::now();
-        for(long long int i(0); i < size_vector; ++i)
-            for(int j=0; j < SIZE; j++)
-              numeric::exp<typename Solver::value_type, Solver::n_>(vec_output_serial[i].data[j], vec_input[i].data[j]);
-//        boost::chrono::nanoseconds ns2 = boost::chrono::high_resolution_clock::now() - start2;
-
-//        std::cout << " numeric::exp<sizeof(T)"<<sizeof(typename Solver::value_type)<<", "<< Solver::n_ <<  ">  vec_exp " << ns3.count()  << " [s], serial_exp "  << ns2.count() << " [s], std::exp "  << ns1.count() << " [s] " << std::endl;
-
+       t.stop();
+       std::cout << Solver::name() << ", " << t.get() << " [s] " <<  std::endl;
     }
 };
 
     int main(int argc, char * argv[]){
         //generate all variants, we call the operator() defined previously
-        boost::mpl::for_each<solver_exp_list>(test_case());
+        boost::mpl::for_each<Solver_op_list_2>(test_case());
         return 0;
     }
