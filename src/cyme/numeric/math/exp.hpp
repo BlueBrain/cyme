d04308665bb521052dad8e27b02a8e45de1c2579
Ewart Timothée <ewart@bluebrain12.epfl.ch>
2013-10-08 13:51:22 +0200

    test exp fam

--- src/cyme/numeric/math/exp.hpp
+++ src/cyme/numeric/math/exp.hpp
@@ -131,6 +131,9 @@ namespace numeric{
 //          return pow<T,n>(a)*T(coeff_remez<T,n>::coeff()) + helper_remez_exp<T,n-1>::exp(a); // classic implementation very slow as pow is recalculating
 //          for every iteration, not optimium as 20 is hardcoded
             return  T(coeff_remez<T,coeff_remez_number::value-n>::coeff()) + helper_remez_exp<T,n-1>::exp(a)*a;
+#ifdef __FMA
+            return muladd(helper_remez_exp<T,n-1>::exp(a),a,T(coeff_remez<T,coeff_remez_number::value-n>::coeff()))
+#endif
         }
     };
 
@@ -138,6 +141,10 @@ namespace numeric{
     struct helper_remez_exp<T,0>{
         static inline T exp(T const& a){
             return T(coeff_remez<T,coeff_remez_number::value>::coeff());
+#ifdef __FMA
+
+#endif
+
         }
     };
 
