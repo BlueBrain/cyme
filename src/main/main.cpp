e6e8319900922c5226c9cfa7f884a9cf53091cd5
Ewart Timothée <ewart@bbpviz1.cscs.ch>
2013-10-09 13:56:20 +0200

    one more bench

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -10,7 +10,7 @@ template<class Ba, class Bb> // m and n are differents into the block that why I
 void init(Ba& block_a, Bb& block_b){
     for(int i=0; i<block_a.number_block(); ++i)
         for(int j=0; j<block_a.size_block(); ++j){
-            typename Ba::value_type random = drand48();
+            typename Ba::value_type random = 10*drand48();
             block_a(i,j) = random;
             block_b(i,j) = random;
         }
@@ -56,14 +56,30 @@ void bench3(memory::block<T,M,N,O>& a){
     }
 }
 
+template<class T, std::size_t M, std::size_t N, memory::order O>
+void bench4(memory::block<T,M,N,O>& a){
+
+    typename memory::block<T,M,N,O>::iterator it;
+
+    for(it = a.begin(); it != a.end(); ++it){
+        (*it)[0] = numeric::e((*it)[4]);
+        (*it)[1] = numeric::e((*it)[5]);
+        (*it)[2] = numeric::e((*it)[6]);
+        (*it)[3] = numeric::e((*it)[7]);
+    }
+}
+
 void print(long t1, long t2, std::string name){
     std::cout << "bench: " << name <<", time [cycle] " << t2-t1 << std::endl;
 }
+
+#define TYPE float 
+
 // no mpl it will not compile on bgq
 int main(int argc, char* argv[]){
 
-    memory::block<double,8,1024, memory::AoS> block_aos;
-    memory::block<double,8,1024, memory::AoSoA> block_aosoa;
+    memory::block<TYPE,8,1024, memory::AoS> block_aos;
+    memory::block<TYPE,8,1024, memory::AoSoA> block_aosoa;
 
     srand(-1);
 
@@ -107,5 +123,17 @@ int main(int argc, char* argv[]){
 
     print(t1,t2,"unroll aosoa");
 
-   // check(block_aos,block_aosoa);
+    t1 = rdtsc();
+        bench4(block_aos);
+    t2 = rdtsc();
+
+    print(t1,t2,"exp aos");
+
+    t1 = rdtsc();
+        bench4(block_aosoa);
+    t2 = rdtsc();
+
+    print(t1,t2,"exp aosoa");
+
+//    check(block_aos,block_aosoa);
 }
