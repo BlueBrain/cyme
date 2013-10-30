596ee1d137eb388af4695f6372657e8fd4b6aa7d
ewart <timothee.ewart@epfl.ch>
2013-10-30 10:25:15 +0100

    clean up, renaming

--- src/cyme/memory/array.hpp
+++ src/cyme/memory/array.hpp
@@ -25,21 +25,171 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-#ifndef CYME_ARRAY_HPP 
-#define CYME_ARRAY_HPP
+#ifndef CYME_BLOCK_HPP 
+#define CYME_BLOCK_HPP
 
-#include "memory/block.hpp"
+#include "memory/detail/simd.h"
+#include "memory/detail/array.hpp" // copy of boost::array + align
+#include "memory/detail/storage.hpp"
+
+namespace memory{
+
+    /* \cond I do not need this part in the doc*/
+    // declaration for the partial specialization onlye
+    // M = #element into the mechanism, N = #number of synapse, Order SoA or SoAoS
+    // or M size of block, N number of block
+    template<class T, std::size_t M, std::size_t N, memory::order O>
+    class block{
+    };
+    /* \endcond */
+
+    /**
+        \brief Block of the memory (partial specialization) is instantiated following  AoS layout
+        T is the type, M the size of the subblock and N the total number of subblock. For AoS  specialization
+        datas are contiguous block after block. I just instantiate a boost::array of block
+    */
+    template<class T, std::size_t M, std::size_t N>
+    class block<T,M,N,AoS> : public boost::array<storage<T,M,AoS>,N> {
+    public:
+        typedef std::size_t                    size_type;
+        typedef T                              value_type; 
+        typedef value_type&                    reference;
+        typedef const value_type&              const_reference;
+        typedef storage<T,M,AoS>               storage_type;
+        typedef boost::array<storage_type,N>   base_type; //default template seems impossible on partial specialization
+        typedef typename  base_type::iterator  iterator;
+       
+        /**
+        \brief Default constructor, the block is set up to 0
+        */
+        explicit block(){
+            for(size_type i(0); i<N; ++i)
+                base_type::operator[](i) = storage_type(); // fill up to 0
+        }
+        
+        /**
+        \brief set up the block to the desired value
+        */
+        block(value_type value){
+            for(size_type i(0); i<N; ++i)
+                base_type::operator[](i) = storage_type(value); // fill up to value
+        }
+        
+        /**
+        \brief return the value of the block i, element j, write only
+        */
+        inline reference operator()(size_type i, size_type j){
+            BOOST_ASSERT_MSG( i < N, "out of range: block AoS i" );
+            BOOST_ASSERT_MSG( j < M, "out of range: block AoS j" );
+            return base_type::operator[](i)(j);
+        }
+
+        /**
+        \brief return the value of the subblock i, element j, read only
+        */
+        inline const_reference operator()(size_type i, size_type j) const{
+            BOOST_ASSERT_MSG( i < N, "out of range: block AoS i" );
+            BOOST_ASSERT_MSG( j < M, "out of range: block AoS j" );
+            return base_type::operator[](i)(j);
+        }
+
+        /**
+        \brief return the number of subblock into the full block
+        */
+        static inline size_type number_block() {
+            return N;
+        }
+
+        /**
+        \brief return the size of basic subblock 
+        */
+        static inline size_type size_block() {
+            return M;
+        }
+    
+    };
+
+    /**
+        \brief Block of the memory (partial specialization) is instantiated following  AoSoS layout
+        T is the type, M the size of the subblock and N the total number of subblock. For AoS  specialization
+        datas are contiguous and interleaved. The boost::array is instantiated but the size depends of the SIMD registers.
+        It is calculated during compile time.
+    */
+    template<class T, std::size_t M, std::size_t N>
+    class block<T,M,N,AoSoA> : public boost::array<storage<T,__GETSIMD__()/sizeof(T)*M,AoSoA>, N/(__GETSIMD__()/sizeof(T))+1>{
+    public:
+        typedef std::size_t                                               size_type;
+        typedef T                                                         value_type;
+        typedef value_type&                                               reference;
+        typedef const value_type&                                         const_reference;
+        typedef storage<T,__GETSIMD__()/sizeof(T)*M,AoSoA>                storage_type;
+        typedef boost::array<storage_type,N/(__GETSIMD__()/sizeof(T))+1>  base_type; //default template seems impossible on partial specialization
+        typedef typename  base_type::iterator                             iterator;
+
+        /**
+        \brief Default constructor, the block is set up to 0
+        */
+        explicit block(){
+            for(size_type i(0); i<N/(__GETSIMD__()/sizeof(T))+1; ++i)
+                base_type::operator[](i) = storage_type(); // fill up to 0
+        }
+
+        /**
+        \brief set up the block to the desired value
+        */
+        block(value_type value){
+            for(size_type i(0); i<N/(__GETSIMD__()/sizeof(T))+1; ++i)
+                base_type::operator[](i) = storage_type(value); // fill up to value
+        }
+
+        /**
+        \brief return the value of the block i, element j, write only
+        */
+        inline reference operator()(size_type i, size_type j){
+            BOOST_ASSERT_MSG( i < N, "out of range: block AoSoA i" );
+            BOOST_ASSERT_MSG( j < M, "out of range: block AoSoA j" );
+            // Please tune me ! (does it exist an alternative to this ? ^_^
+            return base_type::operator[]((i*M+j)/(M*__GETSIMD__()/sizeof(T)))                         //(i)
+                                        (j*(__GETSIMD__()/sizeof(T)) + i%(__GETSIMD__()/sizeof(T)));  //(j)
+        };
+
+        /**
+        \brief return the value of the block i, element j, read only
+        */
+        inline const_reference operator()(size_type i, size_type j) const{
+            BOOST_ASSERT_MSG( i < N, "out of range: block AoSoA i" );
+            BOOST_ASSERT_MSG( j < M, "out of range: block AoSoA j" );
+            // Please tune me ! (does it exist an alternative to this ? ^_^
+            return base_type::operator[]((i*M+j)/(M*__GETSIMD__()/sizeof(T))) //(i)
+                                        (j*(__GETSIMD__()/sizeof(T)) + i%(__GETSIMD__()/sizeof(T)));  //(j)
+        };
+
+        /**
+        \brief return the number of subblock into the full block
+        */
+        static inline size_type number_block() {
+            return N;
+        }
+
+        /**
+        \brief return the size of basic subblock 
+        */
+        static inline size_type size_block() {
+            return M;
+        }
+    };
+}
 
 namespace cyme {
     /*
-    \brief  This class facilitates the creation of an array of synapses (or whatever), the condition the class
+     \brief  This class facilitates the creation of an array of synapses (or whatever), the condition the class
      must encapsulate the basic type (value_type) and the size (value_size) of the basic object under the form:
      template <class T>
      class example{
-        typedef T value_type;
-        static const int value_size = 5;
+     typedef T value_type;
+     static const int value_size = 5;
      }
-    */
+     */
     template<class T, std::size_t N, memory::order O>
     class array : public memory::block<typename T::value_type, T::value_size, N, O>{ };
 }
