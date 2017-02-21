3784b9e0441e9bc1bca223e1cc9742a724bbda97
tim <timothee.ewart@epfl.ch>
2017-02-21 15:04:56 +0100

    > wrapper

--- cyme/core/expression/expr_vec.hpp
+++ cyme/core/expression/expr_vec.hpp
@@ -244,6 +244,21 @@ class vec_lt {
     forceinline vec_simd<T, O, N> operator()() const { return op1() < op2(); }
 };
 
+    /** less than vertex in the DAG from a > b
+     /note the inequality in SIMD are like usual operation it return a register 0 false -1 true
+     */
+    template <class T, cyme::simd O, int N, class OP1, class OP2>
+    class vec_ht {
+        typename vec_traits<OP1, O, N>::value_type op1;
+        typename vec_traits<OP2, O, N>::value_type op2;
+
+    public:
+        forceinline vec_ht(OP1 const &a, OP2 const &b) : op1(a), op2(b) {}
+        /* always return int */
+        forceinline vec_simd<T, O, N> operator()() const { return op1() > op2(); }
+    };
+
+
 /** less than vertex in the DAG from a < b
  /note the inequality in SIMD are like usual operation it return a register 0 false -1 true
  */
